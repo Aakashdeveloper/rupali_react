f55a174e7991706bd8816cce5684d57571028bb4
AAKASH HANDA <AAKASH HANDA>
2018-06-07 07:59:13 +0530

    A

---
+++ musicApp/src/components/artistList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const ArtistsList = (props) => {
+    console.log(props);
+
+    const list = ({allArtists})=>{
+        if(allArtists){
+        return allArtists.map((item) => {
+
+            const style={
+                background:`url('/images/covers/${item.cover}.jpg') no-repeat`
+            }
+
+           
+            
+        })
+    }
+    }
+
+    return(
+        <div className="artists_list">
+            <h4>Artists List</h4>
+            {list(props)}
+        </div>
+    )
+}
+
+export default ArtistsList;
--- musicApp/src/components/home.js
+++ musicApp/src/components/home.js
@@ -1,12 +1,35 @@
 import React, {Component} from 'react';
 
 import Banner from '../components/banner';
+import ArtistList from './artistList'
+
+const URL_ARTISTS = "http://localhost:7666/artists"
 
 class Home extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            artists:''
+        }
+    }
+
+    componentDidMount(){
+        fetch(URL_ARTISTS,{
+            method:'GET'
+        }).then((response)=> response.json())
+        .then((json)=>{
+            this.setState({
+                artists:json
+            })
+        })
+    }
+
     render(){
         return(
             <div>
                 <Banner></Banner>
+                <ArtistList allArtists={this.state.artists}></ArtistList>
             </div>
         )
     }
